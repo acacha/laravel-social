e7af1a3366b87d8f375f8bb308c4f06cffc335a4
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-02-13 17:08:26 +0100

    Sync

--- database/migrations/2014_10_12_500000_adapt_users_table_to_social_login.php
+++ database/migrations/2014_10_12_500000_adapt_users_table_to_social_login.php
@@ -18,7 +18,7 @@ class AdaptUsersTableToSocialLogin extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('password')->nullable()->change();
-            $table->string('email')->unique()->nullable()->change();;
+            $table->string('email')->nullable()->change();;
         });
     }
 
